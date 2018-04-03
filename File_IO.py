80eacb0164ecb604855d5b5e5ff893ad0e032837
FatherLuis <fatherluis98@gmail.com>
2018-04-03 12:05:36 -0500

    Functionable
    Program satifies the requirements. All python files are written as classes. I added a sample file that is require to run the program. All that's left is to add a few try-catch and comment on the program.

--- File_IO.py
+++ File_IO.py
@@ -6,21 +6,56 @@ import re
 class File_IO:
 
     def __init__(self):
-        self.file = filedialog.askopenfilename()
-
-        self.fileName = os.path.basename(self.file)
+        self.fileName = ''
 
         self.startingState = 0
         self.acceptState = 0
 
         self.RuleBook = [[], []]
 
-    def readFile(self):
+
+    def openFile(self):
+
+        self.file = filedialog.askopenfilename()
+        self.fileName = os.path.basename(self.file)
+
+
+    def __createRuleBook(self, lstLines):
 
         paramState = r'[\d*]+'
         param1 = r'(?<=\()\s*\d*'
         param2 = r'(?<=\,)\s*\w*'
         param3 = r'(?<=\=)\s*\d*'
+
+        numLines = int((len(lstLines) - 3) / 2.0)
+
+        #############################################################################
+        for i in range(numLines):
+            self.RuleBook[0].append(0)
+            self.RuleBook[1].append(0)
+        #############################################################################
+
+        for i in range(3, len(lstLines)):
+
+            if re.search(param2, lstLines[i]).group() == 'a':
+
+                self.RuleBook[0][int(re.search(param1, lstLines[i]).group()) - 1] = int(
+                    re.search(param3, lstLines[i]).group())
+
+            else:
+
+                self.RuleBook[1][int(re.search(param1, lstLines[i]).group()) - 1] = int(
+                    re.search(param3, lstLines[i]).group().strip())
+
+        self.startingState = int(re.search(paramState, lstLines[0]).group())
+        self.acceptState = int(re.search(paramState, lstLines[1]).group())
+
+    def readFile(self):
+        #
+        # paramState = r'[\d*]+'
+        # param1 = r'(?<=\()\s*\d*'
+        # param2 = r'(?<=\,)\s*\w*'
+        # param3 = r'(?<=\=)\s*\d*'
 
         try:
 
@@ -31,44 +66,32 @@ class File_IO:
             for line in file:
                 lineString.append(line)
 
-            print(lineString)
-
-            numLines = (len(lineString) - 3) / 2.0
-            numLines = int(numLines)
-
-            #############################################################################
-            for i in range(numLines):
-                self.RuleBook[0].append(0)
-                self.RuleBook[1].append(0)
-            #############################################################################
-
-            print(self.RuleBook)
-
-            for i in range(3, len(lineString)):
-
-                if re.search(param2, lineString[i]).group() == 'a':
-
-                    self.RuleBook[0][int(re.search(param1, lineString[i]).group()) - 1] = int(
-                        re.search(param3, lineString[i]).group())
-
-                else:
-
-                    self.RuleBook[1][int(re.search(param1, lineString[i]).group()) - 1] = int(
-                        re.search(param3, lineString[i]).group().strip())
-
-            print(self.RuleBook)
-
-            self.startingState = int(re.search(paramState, lineString[0]).group())
-            self.acceptState = int(re.search(paramState, lineString[1]).group())
+            self.__createRuleBook(lineString)
 
 
 
-
-
-
-
-
-
+            # numLines = int((len(lineString) - 3) / 2.0)
+            #
+            # #############################################################################
+            # for i in range(numLines):
+            #     self.RuleBook[0].append(0)
+            #     self.RuleBook[1].append(0)
+            # #############################################################################
+            #
+            # for i in range(3, len(lineString)):
+            #
+            #     if re.search(param2, lineString[i]).group() == 'a':
+            #
+            #         self.RuleBook[0][int(re.search(param1, lineString[i]).group()) - 1] = int(
+            #             re.search(param3, lineString[i]).group())
+            #
+            #     else:
+            #
+            #         self.RuleBook[1][int(re.search(param1, lineString[i]).group()) - 1] = int(
+            #             re.search(param3, lineString[i]).group().strip())
+            #
+            # self.startingState = int(re.search(paramState, lineString[0]).group())
+            # self.acceptState = int(re.search(paramState, lineString[1]).group())
 
         except:
             print('eh')
@@ -77,3 +100,4 @@ class File_IO:
         else:
 
             file.close()
+
--- MainGUI.py
+++ MainGUI.py
@@ -6,11 +6,26 @@ class MainGUI:
 
     def __btnEnter_click(self,event):
         print('Entered')
+        referee = Analyser(self.rules.startingState,self.rules.acceptState,self.rules.RuleBook)
+
+        if(referee.vocabCheck(self.txtUserInput.get())):
+            self.lblStatus.config(text='Word is accepted')
+        else:
+            self.lblStatus.config(text='Word is not accepted!!')
+
+
 
     def __btnUpLoad_click(self,event):
         print('Uploaded')
+        self.rules.openFile()
+        self.rules.readFile()
+
+        self.lbl1.config(text= self.rules.fileName)
 
     def __init__(self):
+
+        self.rules = File_IO()
+
         self.MainGUI = tk.Tk()
         self.MainGUI.title('Finite Autonoma')
         self.MainGUI.geometry('400x300')
@@ -20,14 +35,17 @@ class MainGUI:
         btnUpload.place(x=10, y=20)
         btnUpload.bind('<ButtonRelease-1>', self.__btnUpLoad_click)
 
-        lbl1 = tk.Label(self.MainGUI, height=2, text='No File Selected')
-        lbl1.place(x=150, y=22)
+        self.lbl1 = tk.Label(self.MainGUI, height=2, text='No File Selected')
+        self.lbl1.place(x=150, y=22)
 
         lbl2 = tk.Label(self.MainGUI, height=2, text='Enter Encryption:')
         lbl2.place(x=10, y=140)
 
-        txtUserInput = tk.Entry(self.MainGUI, width=30, )
-        txtUserInput.place(x=120, y=150)
+        self.lblStatus = tk.Label(self.MainGUI, height=2, text='')
+        self.lblStatus.place(x=120, y=120)
+
+        self.txtUserInput = tk.Entry(self.MainGUI, width=30, )
+        self.txtUserInput.place(x=120, y=150)
 
         btnEnter = tk.Button(self.MainGUI, width=20, height=2, text='Enter')
         btnEnter.place(x=120, y=210)
